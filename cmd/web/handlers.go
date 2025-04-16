ba0456c826c916f995290657ef870ffdd99dfb1a
srivastavya02 <srivastavya24@gmail.com>
2025-04-16 10:39:09 +0530

    added some ui

---
+++ cmd/web/handlers.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"text/template"
+	"log"
+)
+
+func home(w http.ResponseWriter, r *http.Request)	{
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return 
+	}
+
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+		}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return 
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request)	{
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id<1 {
+		http.NotFound(w, r)
+		return 
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with id %d", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request)	{
+	if r.Method != "POST" {
+
+		w.Header().Set("Allow", http.MethodPost)
+		
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return 
+	}
+
+	w.Write([]byte("Create a snippet"))
+}
