ad654bd9f65727e6321e54cb04033d4eda3793a5
srivastavya02 <srivastavya24@gmail.com>
2025-04-18 14:59:03 +0530

    connecting to pg db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,98 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"database/sql"
+	
+	_"github.com/lib/pq"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "postgres://web:pass@localhost/snippetbox?sslmode=disable", "PostgreSQL data source name")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate | log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+	errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
+	app := &application {
+		errorLog: errorLog,
+		infoLog: infoLog,
+	}
+
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+			return nil, err
+	}
+
+	s, err := f.Stat()
+	if err != nil {
+			return nil, err
+	}
+	
+	if s.IsDir() {
+			index := filepath.Join(path, "index.html")
+			if _, err := nfs.fs.Open(index); err != nil {
+					closeErr := f.Close()
+					if closeErr != nil {
+							return nil, closeErr
+					}
+
+					return nil, err
+			}
+	}
+
+	return f, nil
+} 
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+	return nil, err
+	}
+	if err = db.Ping(); err != nil {
+	return nil, err
+	}
+	return db, nil
 }
